dcd6ad526483d3b35ae4912c213e6bb60fd007ab
Yohsuke Yamakawa <yyamakawa@gmail.com>
2019-01-22 18:09:34 -0500

    unit testing games
    using mocha to unit test game

---
+++ test/game_test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function (){
+	describe('checkGameStatus', function(){
+		it('should tell me when the game is over');
+	});
+});
